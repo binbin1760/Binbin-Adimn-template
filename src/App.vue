953cd052e945788e59364b6d5f989e8a1f8ab57f
廖顺彬 <569279172@qq.com>
2023-08-03 18:33:54 +0800

    feat: alova

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <layout>
     <template v-slot:side>
-      <div class="side">鼠鼠我啊，自己做模板了</div>
+      <div class="side">
+        <div class="title">Binbin-admin</div>
+        <n-config-provider :theme="darkTheme">
+          <n-menu :options="menuOptions" />
+        </n-config-provider>
+      </div>
     </template>
     <template v-slot:content> </template>
   </layout>
@@ -9,15 +14,26 @@
 
 <script setup lang="ts">
 import layout from "./components/layout.vue";
+import type { MenuOption } from "naive-ui";
+import { darkTheme } from "naive-ui";
+
+const menuOptions: MenuOption[] = [
+  {
+    whateverLabel: "且听风吟",
+    whateverKey: "hear-the-wind-sing",
+  },
+];
 </script>
 
 <style scoped>
 .side {
   box-sizing: border-box;
-  padding: 1rem;
   font-size: 1.8rem;
   background-color: rgb(0, 20, 40);
   color: #ffff;
   height: 100vh;
+}
+.title {
+  padding: 1rem;
 }
 </style>
