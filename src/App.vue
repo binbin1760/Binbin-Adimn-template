83bfadd03ec0593f6a1559ae862dcdc062430ef9
廖顺彬 <569279172@qq.com>
2023-08-03 14:07:46 +0800

    feat: vite Config

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,21 @@
     <template v-slot:side>
       <div class="side">鼠鼠我啊，自己做模板了</div>
     </template>
-    <template v-slot:content>
-    </template>
+    <template v-slot:content> </template>
   </layout>
 </template>
 
 <script setup lang="ts">
-import layout from './components/layout.vue';
+import layout from "./components/layout.vue";
 </script>
 
 <style scoped>
 .side {
+  box-sizing: border-box;
   padding: 1rem;
   font-size: 1.8rem;
   background-color: rgb(0, 20, 40);
-  height: 100%;
   color: #ffff;
+  height: 100vh;
 }
 </style>
--- src/components/layout.vue
+++ src/components/layout.vue
@@ -1,33 +1,24 @@
 <template>
-    <div class="layout">
-        <div class="side">
-            <slot name="side"></slot>
-        </div>
-        <div class="content">
-            <slot name="content"></slot>
-        </div>
+  <div class="layout">
+    <div class="side">
+      <slot name="side"></slot>
     </div>
+    <div class="content">
+      <slot name="content"></slot>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 .layout {
-    display: flex;
-    flex: 1;
-    height: 100vh;
-    overflow-y: scroll;
+  display: flex;
+  height: 100vh;
 }
 
 .layout .content {
-    flex: 1;
-    background-color: aqua;
-}
-
-.a {
-    width: 200px;
-    height: 2000px;
-    background-color: white;
+  flex: 1;
+  background-color: aqua;
 }
 </style>
